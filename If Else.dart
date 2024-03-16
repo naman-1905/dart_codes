33b436856a41acf91069ff76be8475f1068cb2a4
Naman Chaturvedi <namanoj19@gmail.com>
2024-03-16 16:31:10 +0530

    If Else Condition

---
+++ If Else.dart
@@ -0,0 +1,14 @@
+void main(List<String>args){
+  //Defining two integers num1 and num2 and assigning values to them
+  int num1 = 5;
+  int num2 = 7;
+  //Creating an if condition
+  //IF condition checks the argument before further executing the code
+  if (num1 > num2) {
+    print("$num1 is greater than $num2.");
+  }
+  //ELSE condition is used to define the action which is to be performed if the argument in IF is not satisfied. 
+  else{
+    print("The number $num1 is smaller than $num2.");
+  }
+}
