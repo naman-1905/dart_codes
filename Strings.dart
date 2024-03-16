28e69ad1eba9c8bc508eea9802f4b8ecabf4fa03
Naman Chaturvedi <namanoj19@gmail.com>
2024-03-16 16:20:07 +0530

    Second Commit

---
+++ Strings.dart
@@ -0,0 +1,12 @@
+void main(List<String>args){
+  // Different ways to print strings in Dart
+  print("Welcome to the Basics of " + "Dart Programming Language in 2024");
+  print("Hi");
+  print('Hi');
+  // Defining a string to use in the code
+  String country = "India";
+  print (country);
+  print ("country");
+  print("////THANK YOU FOR CODING////");
+  
+}
