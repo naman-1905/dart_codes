43e8abe88c5d7cd4871a036450deed84549137e0
Naman Chaturvedi <namanoj19@gmail.com>
2024-03-16 23:16:08 +0530

    Data Types

---
+++ Data Types.dart
@@ -0,0 +1,23 @@
+void main(List<String>args){
+  // Data Types : Strings, Numbers and Boolean
+  // Numbers data types are of two types: Integers and Decimals
+  // Primitive Data Types are not present in Dart
+  // Boolean (bool) is used for True or False
+  
+  // Integers' Data Types use keyword int.
+  int num1 = 10;
+  print (num1);
+  print ("num1");
+  num1 = 20;
+  print(num1);
+
+  //Decimals use the keyword double
+  double num3 = 2.5;
+  print (num3);
+
+  //Decimals can store the values of integers as well, but the vice versa isn't true.
+  double num4 = 8;
+  print ("The value of the decimal variable num4 is $num4");
+
+ // Syntax for data-types in Flutter: <type> <variable name> = <value>;
+}
