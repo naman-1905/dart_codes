e1142fff0a90e1f84347a935a43a4d66926bd0f8
Naman Chaturvedi <namanoj19@gmail.com>
2024-03-16 16:04:15 +0530

    First Commit

---
+++ .vscode/Print Strings.dart
@@ -0,0 +1,9 @@
+void main(List<String>args){
+  print("Hello to Dart's Coding side");
+  print("Hi");
+  print("1+2+3");
+  print("Hello"+" World");
+  print(1-1);
+  int num1=5;
+  print(num1);
+}
---
+++ Addition.dart
@@ -0,0 +1,11 @@
+void main(List<String>args){
+  print('1+2');
+  print(1+3);
+  int num1=1;
+  int num2=4;
+  int num3=num1 + num2;
+  print("The sum of two integers num1 and num2 is ");
+  print(num3);
+
+
+}
