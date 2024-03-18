bdb8e3fc37ef55f892f9ee42e83dfa462caeb2df
Naman Chaturvedi <namanoj19@gmail.com>
2024-03-18 22:43:54 +0530

    Comparison Operators

---
+++ Loops.dart
@@ -0,0 +1,18 @@
+void main (List<String>args){
+  // Comparison Operators
+  //&& --> AND Operator (two conditions are satisfied)
+  //|| --> OR Operator (one of the conditions is satisfied)
+  //! --> NOT Operator (value1 is not equal to value2)
+
+  int num1 = 23;
+  int num2 = 20;
+  int num3 = 12;
+  if(num1==num2 && num2==num3){
+    print("Number 1 is equal to Number 3.");
+  }
+  else{
+    print("Not Equal.");
+  }
+
+
+}
