2fb57e182ac7c8cd219e55c438288b4b19570927
Naman Chaturvedi <namanoj19@gmail.com>
2024-03-18 22:56:39 +0530

    Loops

---
+++ Loops.dart
@@ -0,0 +1,26 @@
+void main (List<String>args){
+  // For Loop
+  // Print 1 to 100
+  for (int i = 0;
+  i <= 100; 
+  i++){
+    print(i);
+  }
+
+  // While Loop :- The Situation is checked first and then looped
+  // Print Nos. less than 5 (till 4)
+  int num=1;
+  while (num < 5){
+    print (num);
+    num++;
+  }
+
+  // Do While Loop :- State condition first then we check the situation
+  // Print Nos. till 5 (Upto 5)
+  int num2 = 0;
+  do{
+    print(num2);
+    num2++;
+  }while (num2<=5);
+  }
+
