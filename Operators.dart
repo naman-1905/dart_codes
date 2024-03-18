08f345c53b20c23ed96556ea585bfe74fd22d52a
Naman Chaturvedi <namanoj19@gmail.com>
2024-03-18 22:28:54 +0530

    Operators

---
+++ Operators.dart
@@ -0,0 +1,17 @@
+void main(List<String>args){
+  // Arithmetic Operators in Dart are +, -, *. / and %(modulo).
+  double num3=3;
+  num3 = num3 + 7;
+  print(num3);
+
+  //Another way of operators can be += for adding and assigning at same time
+  //Called Assignment Operators.
+  num3 += 7;
+  print(num3);
+
+  //Equality Operators and Relational Operators
+  // == Equals
+  // != Not Equal
+  // <, >, Greater, Less
+  // <= and >=  Less than or equal to, Greater than or equal 
+}
